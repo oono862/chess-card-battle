d930853da7cf39654175a10f41aa919ce743807a
Your Name <hiroki_ono@ygcollege.jp>
2026-01-08 13:51:34 +0900

    Optimize startup: lazy-load chess piece images and lazy-init pygame; add startup_optimizer

--- C.C.B/CardGame.py
+++ C.C.B/CardGame.py
@@ -273,7 +273,13 @@ try:
     from ui.window import initialize_window, get_screen, get_clock, get_window_size, update_window_size
 except Exception:
     logger.exception("Failed to import ui.window module")
-    pygame.init()
+    try:
+        from c.c.b.utils.startup_optimizer import lazy_init_pygame
+    except Exception:
+        def lazy_init_pygame():
+            import pygame as _pygame
+            _pygame.init()
+    lazy_init_pygame()
     def initialize_window(w=1200, h=800, caption="Chess-Card-Battle", resizable=True):
         screen = pygame.display.set_mode((w, h), pygame.RESIZABLE if resizable else 0)
         pygame.display.set_caption(caption)
@@ -308,7 +314,13 @@ try:
 except Exception:
     logger.exception("Failed to import ui modules")
     BASE_UI_W, BASE_UI_H = 1200, 800
-    pygame.init()
+    try:
+        from c.c.b.utils.startup_optimizer import lazy_init_pygame
+    except Exception:
+        def lazy_init_pygame():
+            import pygame as _pygame
+            _pygame.init()
+    lazy_init_pygame()
     FONT = pygame.font.SysFont("Noto Sans JP, Meiryo, MS Gothic", 20)
     SMALL = pygame.font.SysFont("Noto Sans JP, Meiryo, MS Gothic", 18)
     TINY = pygame.font.SysFont("Noto Sans JP, Meiryo, MS Gothic", 16)
